b0ffa9b0b6beb8ee89321d742808504628e89345
JefersonDuarte <jeferson_duartemk@outlok.com>
2021-07-25 23:56:39 -0300

    feat(frontend): init pokeapi structure

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -1,1 +1,38 @@
-console.log('Init');
+const PokeApi = {
+    init: () => {
+        PokeApi.getDataPokeApi();
+    }, getDataPokeApi: () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=100').then(response => {
+            return response.json();
+        }).then(response => {
+            PokeApi.mountElement(response.results)
+            console.log('PokeApi.init();');
+        })
+
+    }, mountElement: (response) => {
+        response.map(item => {
+            var name = item.name;
+            $("#pokemons").append(
+                `<option class="pokemons" value="${name}">${name}</option>`
+            );
+        });
+
+        PokeApi.getPokemonData();
+
+    }, getPokemonData: () => {
+        var $pokemons = $('#pokemons');
+
+        $pokemons.on('change', function() {
+            var id =  $(this).find(":selected").val(),
+            url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+            fetch(url).then(response => {
+                return response.json();
+            }).then(response => {
+                console.log(response);
+            })
+        });
+    }
+}
+
+PokeApi.init();
